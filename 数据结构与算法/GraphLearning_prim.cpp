e652af77a59c6abfd5ba2dc58307466128017998
zouzpgit <1162645284@qq.com>
2019-04-25 13:11:15 +0800

    代码提交信息

---
+++ 数据结构与算法/GraphLearning_prim.cpp
@@ -0,0 +1,211 @@
+﻿// GraphLearning.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "GraphLearning.h"
+
+
+#include<iostream>
+#include<math.h>
+#include<vector>
+#include<cstring>
+#include<assert.h>
+#include<algorithm>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+#define INFINITY 10000
+
+
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+
+template<typename T>
+
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(NULL);
+
+	if (hModule != NULL)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+		{
+			// TODO: change error code to suit your needs
+			_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+			nRetCode = 1;
+		}
+		else
+		{
+			// TODO: code your application's behavior here.
+		}
+	}
+	else
+	{
+		// TODO: change error code to suit your needs
+		_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
+
+void swap(int *data1, int *data2)
+{
+	int temp;
+	temp = *data1;
+	*data1 = *data2;
+	*data2 = temp;
+}
+
+void BubbleSort2(int *L,int length)
+{
+	int i, j;
+	/* flag⽤来作为标记 */
+	bool flag = TRUE;
+	/* 若flag为true说明有过数据交换，否则停⽌循环 */
+	for (i = 1; i <length && flag; i++)
+	{
+		/* 初始为false */
+		flag = FALSE;
+		for (j =length - 1; j >= i; j--)
+		{
+			if (L[j-1] > L[j])
+			{
+				/* 交换L->r[j]与L->r[j-1]的值 */
+				swap(&L[j] ,&L[j - 1]);
+				/* 如果有数据交换，则flag为true */
+				flag = TRUE;
+			}
+		}
+	}
+}
+
+/* 建⽴⽆向⽹图的邻接矩阵表示 */
+void CreateMGraph(MGraph *G)
+{
+	int i, j, k, w;
+	cout<<"输入顶点数和边数:" <<endl;
+	/* 输⼊顶点数和边数 */
+	cin >> G->numVertexes >> G->numEdges;
+	/* 读⼊顶点信息，建⽴顶点表 */
+	for (i = 0; i < G->numVertexes; i++)
+		cin >> G->vexs[i];
+		//scanf(&G->vexs[i]);
+	for (i = 0; i < G->numVertexes; i++)
+		for (j = 0; j <G->numVertexes; j++)
+			/* 邻接矩阵初始化 */
+			if (i == j)
+			{
+				G->arc[i][j] = 0;    //对角线上的数据为0
+			}
+			else
+				G->arc[i][j] = 10000;
+	/* 读⼊numEdges条边，建⽴邻接矩阵 */
+	for (k = 0; k < G->numEdges; k++)
+	{
+		cout << "输入边(vi,vj)上的下标i，下标j和权w:" << endl;
+		/* 输⼊边(vi,vj)上的权w */
+		cin >> i >> j >> w;
+
+		G->arc[i][j] = w;
+		/* 因为是⽆向图，矩阵对称 */
+		G->arc[j][i] = G->arc[i][j];
+	}
+}
+
+/* Prim算法⽣成最⼩⽣成树 */
+void MiniSpanTree_Prim(MGraph G)
+{
+	int min, i, j, k;
+
+	/* 保存相关顶点下标 */
+	int adjvex[MAXVEX] = {0};
+	/* 保存相关顶点间边的权值 */
+	int lowcost[MAXVEX] = {0};
+	/* 初始化第⼀个权值为0，即v0加⼊⽣成树 */
+	/* lowcost的值为0，在这⾥就是此下标的顶点已经加⼊⽣成树 */
+	lowcost[0] = 0;
+	/* 初始化第⼀个顶点下标为0 */
+	adjvex[0] = 0;
+	/* 循环除下标为0外的全部顶点 */
+	for (i = 1; i < G.numVertexes; i++)
+	{
+		/* 将v0顶点与之有边的权值存⼊数组 */
+		lowcost[i] = G.arc[0][i];
+		/* 初始化都为v0的下标 */
+		adjvex[i] = 0;
+	}
+	for (i = 1; i < G.numVertexes; i++)
+	{
+		/* 初始化最⼩权值为∞， */
+		/* 通常设置为不可能的⼤数字如32767、65535等 */
+		min = 10000;
+		j = 1; k = 0;
+
+		// add  找终点
+		/* 循环全部顶点 */
+		while (j < G.numVertexes)
+		{
+			/* 如果权值不为0且权值⼩于min */
+			if (lowcost[j] != 0 && lowcost[j] < min)
+			{
+				/* 则让当前权值成为最⼩值 */
+				min = lowcost[j];
+				/* 将当前最⼩值的下标存⼊k */
+				k = j;
+			}
+			j++;
+		}
+		/* 打印当前顶点边中权值最⼩边 */
+		printf("(%d,%d)", adjvex[k], k);
+		/* 将当前顶点的权值设置为0，表示此顶点已经完成任务 */
+		lowcost[k] = 0;
+
+		// add  找起点
+		/* 循环所有顶点 */
+		for (j = 1; j < G.numVertexes; j++)
+		{
+			/* 若下标为k顶点各边权值⼩于此前这些顶点未被加⼊⽣成树权值 */
+			if (lowcost[j] != 0 && G.arc[k][j] < lowcost[j])
+			{
+				/* 将较⼩权值存⼊lowcost */
+				lowcost[j] = G.arc[k][j];
+				/* 将下标为k的顶点存⼊adjvex */
+				adjvex[j] = k;
+			}
+		}
+	}
+}
+
+int main()
+{
+	MGraph mgraph;
+
+	
+	//初始化二维数组
+	for (int i = 0; i < MAXVEX; i++)
+	{
+		for (int j = 0; j < MAXVEX; j++)
+		{
+			mgraph.arc[i][j] = 0;
+		}
+	}
+
+	CreateMGraph(&mgraph);
+
+	MiniSpanTree_Prim(mgraph);
+
+	return 0;
+	//BubbleSort2(a.data(), a.size());
+		
+	
+}
---
+++ 数据结构与算法/Levenberg–Marquardt.cpp
@@ -0,0 +1,229 @@
+﻿// Levenberg–Marquardt.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Levenberg–Marquardt.h"
+
+// A simple demo of Gauss-Newton algorithm on a user defined function
+
+#include <cstdio>
+#include <vector>
+#include <opencv2/core/core.hpp>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(NULL);
+
+	if (hModule != NULL)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+		{
+			// TODO: change error code to suit your needs
+			_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+			nRetCode = 1;
+		}
+		else
+		{
+			// TODO: code your application's behavior here.
+		}
+	}
+	else
+	{
+		// TODO: change error code to suit your needs
+		_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
+
+
+using namespace std;
+using namespace cv;
+
+const double DERIV_STEP = 1e-5;
+const int MAX_ITER = 100;
+
+
+void LM(double(*Func)(const Mat &input, const Mat params), // function pointer
+	const Mat &inputs, const Mat &outputs, Mat params);
+
+double Deriv(double(*Func)(const Mat &input, const Mat params), // function pointer
+	const Mat &input, const Mat params, int n);
+
+// The user defines their function here
+double Func(const Mat &input, const Mat params);
+
+int main()
+{
+	// For this demo we're going to try and fit to the function
+	// F = A*exp(t*B)
+	// There are 2 parameters: A B
+	int num_params = 2;
+
+	// Generate random data using these parameters
+	int total_data = 8;
+
+	Mat inputs(total_data, 1, CV_64F);
+	Mat outputs(total_data, 1, CV_64F);
+
+	//load observation data
+	for (int i = 0; i < total_data; i++) {
+		inputs.at<double>(i, 0) = i + 1;  //load year
+	}
+	//load America population
+	outputs.at<double>(0, 0) = 8.3;
+	outputs.at<double>(1, 0) = 11.0;
+	outputs.at<double>(2, 0) = 14.7;
+	outputs.at<double>(3, 0) = 19.7;
+	outputs.at<double>(4, 0) = 26.7;
+	outputs.at<double>(5, 0) = 35.2;
+	outputs.at<double>(6, 0) = 44.4;
+	outputs.at<double>(7, 0) = 55.9;
+
+	// Guess the parameters, it should be close to the true value, else it can fail for very sensitive functions!
+	Mat params(num_params, 1, CV_64F);
+
+	//init guess
+	params.at<double>(0, 0) = 6;
+	params.at<double>(1, 0) = 0.3;
+
+	LM(Func, inputs, outputs, params);
+
+	printf("Parameters from GaussNewton: %lf %lf\n", params.at<double>(0, 0), params.at<double>(1, 0));
+
+	return 0;
+}
+
+double Func(const Mat &input, const Mat params)
+{
+	// Assumes input is a single row matrix
+	// Assumes params is a column matrix
+
+	double A = params.at<double>(0, 0);
+	double B = params.at<double>(1, 0);
+
+	double x = input.at<double>(0, 0);
+
+	return A*exp(x*B);
+}
+
+//calc the n-th params' partial derivation ， the params are our  final target
+double Deriv(double(*Func)(const Mat &input, const Mat params), const Mat &input, const Mat params, int n)
+{
+	// Assumes input is a single row matrix
+
+	// Returns the derivative of the nth parameter
+	Mat params1 = params.clone();
+	Mat params2 = params.clone();
+
+	// Use central difference  to get derivative
+	params1.at<double>(n, 0) -= DERIV_STEP;
+	params2.at<double>(n, 0) += DERIV_STEP;
+
+	double p1 = Func(input, params1);
+	double p2 = Func(input, params2);
+
+	double d = (p2 - p1) / (2 * DERIV_STEP);
+
+	return d;
+}
+
+void LM(double(*Func)(const Mat &input, const Mat params),
+	const Mat &inputs, const Mat &outputs, Mat params)
+{
+	int m = inputs.rows;
+	int n = inputs.cols;
+	int num_params = params.rows;
+
+	Mat r(m, 1, CV_64F); // residual matrix
+	Mat r_tmp(m, 1, CV_64F);
+	Mat Jf(m, num_params, CV_64F); // Jacobian of Func()
+	Mat input(1, n, CV_64F); // single row input
+	Mat params_tmp = params.clone();     //params就是要估计的量，即X
+
+	double last_mse = 0;
+	float u = 1, v = 2;
+	Mat I = Mat::ones(num_params, num_params, CV_64F);//construct identity matrix
+	int i = 0;
+	for (i = 0; i < MAX_ITER; i++) {
+		double mse = 0;
+		double mse_temp = 0;
+
+		for (int j = 0; j < m; j++) {
+			for (int k = 0; k < n; k++) {//copy Independent variable vector, the year
+				input.at<double>(0, k) = inputs.at<double>(j, k);
+			}
+
+			r.at<double>(j, 0) = outputs.at<double>(j, 0) - Func(input, params);//diff between previous estimate and observation population
+
+			mse += r.at<double>(j, 0)*r.at<double>(j, 0);   //add zzp 实际输出和模型输出的误差的平方和即F(X)
+
+			for (int k = 0; k < num_params; k++) {
+				Jf.at<double>(j, k) = Deriv(Func, input, params, k);  //construct jacobian matrix   add zzp 对估计的函数求导
+			}
+		}
+
+		mse /= m;    //add  最终的F(X)
+		params_tmp = params.clone();
+
+		Mat hlm = (Jf.t()*Jf + u*I).inv()*Jf.t()*r;
+		params_tmp += hlm;
+		for (int j = 0; j < m; j++) {
+			r_tmp.at<double>(j, 0) = outputs.at<double>(j, 0) - Func(input, params_tmp);//diff between current estimate and observation population
+			mse_temp += r_tmp.at<double>(j, 0)*r_tmp.at<double>(j, 0);
+		}
+
+		mse_temp /= m;  //add   最终的F(X+hlm)
+
+		Mat q(1, 1, CV_64F);
+		q = (mse - mse_temp) / (0.5*hlm.t()*(u*hlm - Jf.t()*r));   //  L(0)-L(hlm)=1/2*hlm.t()*[u*hlm-g]
+		double q_value = q.at<double>(0, 0);
+		if (q_value>0)
+		{
+			double s = 1.0 / 3.0;
+			v = 2;
+			mse = mse_temp;    //  将F(X)更新为F(X+hlm)
+			params = params_tmp;    //  add  说明本次迭代的估计值符合要求，下次继续迭代
+			double temp = 1 - pow(2 * q_value - 1, 3);
+			if (s>temp)
+			{
+				u = u * s;
+			}
+			else
+			{
+				u = u * temp;
+			}
+		}
+		else
+		{
+			u = u*v;
+			v = 2 * v;
+			params = params_tmp;
+		}
+
+
+		// The difference in mse is very small, so quit
+		if (fabs(mse - last_mse) < 1e-8) {
+			break;
+		}
+
+		//printf("%d: mse=%f\n", i, mse);
+		printf("%d %lf\n", i, mse);
+		last_mse = mse;
+	}
+}
